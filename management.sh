e38ad0027995336ae1053ca036a380e2aed4ae21
kalashkotwal <kalashkotwal298@gmail.com>
2024-07-24 22:45:30 +0530

    This is my 2nd commit

--- management.sh
+++ management.sh
@@ -1,43 +1,43 @@
-!#/bin/bash
+#!/bin/bash
 
 
-# Display a WELCOME TEXTLINE
-echo "The script has been written by...and is for system health monitoring ."
+# 1. Display a WELCOME TEXTLINE
+echo "This script has been written by KALASH KOTWAL and is for SYSTEM HEALTH MONITORING."
 echo
 
 # 2. Checks for the following
 
 # a) Number of vCPUs
-vcpu_count=$(grep -c processor /proc/cpuinfo)
-echo "Number of vCPUs:$vcpu_count"
+num_vcpus=$(nproc)
+echo "Number of vCPUs: $num_vcpus"
 echo
 
 # b) RAM size and used RAM
-ram_total=$(free -h | awk '/^Mem:/ {print $2}')
-ram_used=$(free -h | awk '/^Mem:/ {print $3}')
-echo "RAM Total: $ram_total"
-echo "RAM Used: $ram_used"
+total_ram=$(free -h | awk '/^Mem:/ {print $2}')
+used_ram=$(free -h | awk '/^Mem:/ {print $3}')
+echo "Total RAM: $total_ram"
+echo "Used RAM: $used_ram"
 echo
 
 # c) Total disk size and used disk size
-disk_total=$(df -h --total | awk '/^total/ {print $2}')
-disk_used=$(df -h --total | awk '/^total/ {print$3}')
-echo "Total Disk Size: $disk_total"
-echo "Used Disk Size: $disk_used"
+total_disk=$(df -h --total | awk '/^total/ {print $2}')
+used_disk=$(df -h --total | awk '/^total/ {print$3}')
+echo "Total Disk Size: $total_disk"
+echo "Used Disk Size: $used_disk"
 echo
 
 # d) Get public and private IP addresses of server
-private_ip=$(hostname -I)
-public_ip=$(curl -s ifconfig.me)
-echo "private IP: $private_ip"
-echo "public IP: $public_ip"
+ip_private=$(hostname -I)
+ip_public=$(curl -s ifconfig.me)
+echo "private IP: $ip_private"
+echo "public IP: $ip_public"
 echo
 
-# e) Get the status of services installed on the server (example: nginx and sshd)
-services=("nginx" "sshd")  # Add more services as needed
+# e) Get the status of services installed on the server (example: Apache2 and Mysql)
+services=("Apache2" "Mysql")  # Add more services as needed
 echo "Service  statuses:"
 for service in "${services[@]}"
 do
-	status=$(systemctcl is-active $services)
-	echo "services: $status"
+	status=$(systemctcl is-active $service)
+	echo "$service: $status"
 done
